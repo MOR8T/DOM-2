6d5a1d5302c625c8b1874e5c74c5193a059eb05b
Shukurov Shahrom <112476380+MOR8T@users.noreply.github.com>
2022-10-20 15:00:08 +0500

    commit1

---
+++ main.js
@@ -0,0 +1,31 @@
+// creatElement()
+
+
+let input = document.createElement("input");
+input.type = "time";
+document.body.appendChild(input);
+
+
+
+
+// // appendChild()
+
+// let h1 = document.createElement("h1");
+// h1.innerHTML = "HELLO";
+// document.body.appendChild(h1);
+
+// let h5 = document.createElement("h5");
+// h5.innerHTML = "HELLO";
+// document.body.appendChild(h5);
+
+
+
+// // classlist()
+// let input2 = document.createElement("input");
+// input2.type = "text";
+// document.body.appendChild(input2);
+
+// input2.classList.add("inn");
+// inn.style.background = "red";
+
+// console.log(document.body)
